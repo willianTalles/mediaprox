aac82b45de3bbc85cc94e986fba9ded172ed8517
Willian Talles Marcolino Dantas <willian.talles@A309-21L.imd.local>
2018-05-24 21:56:31 -0300

    Codigo final!

--- main.cpp
+++ main.cpp
@@ -6,28 +6,19 @@ using std::endl;
 using std::vector;
 #include <numeric>
 using std::accumulate;
+#include <iterator>
+using std::distance;
+#include <algorithm>    // 
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+
+
 
 
 template<typename InputIterator>
 InputIterator closest2mean(InputIterator first, InputIterator last){
-	int soma = accumulate( first, last, 0);
-	int cont = 0;
-	vector<int> diferencas;
-
-
-	soma = soma/cont;
-
-	cont = 0;
-	for (auto i = first; i != last; ++i){
-		diferencas.push_back( ( ( (*i) - soma ) < 0 ) ? ((*i) - soma) * (-1) :  ((*i) - soma) );
-		// cout << diferencas[cont] << " ";
-	}
-
-
-
-	cout << soma << endl;
-
-	return first;
+	return lower_bound (first, last, accumulate( first, last, 0)/distance(first, last) );
 }
 
 int main() {
